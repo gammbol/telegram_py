3cd26c7d9ac0e78c91102a510aedae2744f42d42
gammbol <71922180+gammbol@users.noreply.github.com>
2021-11-28 13:22:48 +0300

    continue making a telegram bot :)

--- bot.py
+++ bot.py
@@ -1,14 +1,23 @@
 import telebot
+from telebot import types
+
 bot  = telebot.TeleBot('2124840185:AAE1nh4vlzNfqRV_hzAjQVUvuZv3HgVQT0s')
 
+#start chatting with bot
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Доброго времени суток! С этого момента, я - ваш помощник\
-        в составлении школьного расписания!')
+    sti = open('welcome_sticker.webp', 'rb')
+    bot.send_sticker(message.chat.id, sti)
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, 'Привет! Чем могу помочь?')
+    bot.send_message(message.chat.id, "Доброго времени суток, <b>{0.first_name}</b>! \nС этого момента, я, <b>{1.first_name}</b>, буду вашим помощником\
+ в составлении школьного расписания!".format(message.from_user, bot.get_me()), parse_mode='html')
+
+    #keyboard
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton('📝 Создать новое расписание')
+    item2 = types.KeyboardButton('✏️ Изменить расписание')
+    markup.add(item1,item2)
+
+    bot.send_message(message.chat.id, 'Давай посмотрим, что тебе нужно:', reply_markup=markup)
+
 bot.polling(none_stop=True, interval=0)
-
