238c1c0a5747b5998135ea64277ee3d4b1ef49fd
gammbol <71922180+gammbol@users.noreply.github.com>
2022-02-13 20:06:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,8 +51,6 @@ action.add(add, delete)
 #start chatting with bot
 @bot.message_handler(commands=['start'])
 def start(message):
-    sti = open('welcome.tgs', 'rb')
-    bot.send_sticker(message.chat.id, sti)
 
     bot.send_message(message.chat.id, "Доброго времени суток, <b>{0.first_name}</b>! \nС этого момента, я, <b>{1.first_name}</b>,\
 буду вашим помощником в составлении школьного расписания!".format(message.from_user, bot.get_me()), parse_mode='html')
